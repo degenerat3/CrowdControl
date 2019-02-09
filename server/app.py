67a9bae286db522cc0ff9a847ec7df2d40c016eb
degenerat3 <james.maskelony@gmail.com>
2019-02-09 08:12:24 -0500

    bugfix

--- server/app.py
+++ server/app.py
@@ -45,7 +45,7 @@ def process_callbacks(host, typ):
 def log_action(hosts, cmds):
     action_time = strftime("%Y-%m-%d %H:%M:%S", gmtime())
     log_str = action_time + " : " + str(hosts) + " : " + cmds
-    with open("/tmp/cc/actions.log", 'a') as f:
+    with open("/tmp/cc/tasks.log", 'a') as f:
         f.write(log_str)
 
 @app.route('/api/commander/push', methods=['POST'])
